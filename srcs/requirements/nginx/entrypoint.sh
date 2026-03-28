c41940913f7665f382cac642a42050f5418600c7
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2026-03-28 11:26:09 +0300

    feat(nginx): add SSL configuration and entrypoint script

--- srcs/requirements/nginx/entrypoint.sh
+++ srcs/requirements/nginx/entrypoint.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+openssl req \
+	-x509 \
+	-nodes \
+	-days 365 \
+	-newkey rsa:2048 \
+	-keyout /ssl/nginx.key \
+	-out /ssl/nginx.crt \
+	-subj "/C=TR/ST=Turkey/L=Kocaeli/O=42/OU=ahekinci/CN=$DOMAIN_NAME"
+
+exec nginx -g "daemon off;"
