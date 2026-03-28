8e79f3164dff2f63d67e61ec0c8eeb1033f3e7a0
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2026-03-28 10:30:13 +0300

    feat(entrypoint): add WordPress setup commands in entrypoint

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
 
+DB_PASSWORD=$(cat /run/secrets/DB_PASSWORD) || {echo "Error: DB_PASSWORD secret not found"; exit 1;}
+DB_ROOT_PASSWORD=$(cat /run/secrets/DB_ROOT_PASSWORD) || {echo "Error: DB_ROOT_PASSWORD secret not found"; exit 1;}
+WP_PASSWORD=$(cat /run/secrets/WP_PASSWORD) || {echo "Error: WP_PASSWORD secret not found"; exit 1;}
+WP_ADMIN_PASSWORD=$(cat /run/secrets/WP_ADMIN_PASSWORD) || {echo "Error: WP_ADMIN_PASSWORD secret not found"; exit 1;}
+
+wp config create \
+	--allow-root \
+	--path=/var/www/html/wordpress \
+	--dbname="${DB_DB}" \
+	--dbuser="${DB_USER}" \
+	--dbpass="${DB_PASSWORD}" \
+	--dbhost=mariadb \
+
+wp core install \
+	--allow-root \
+	--path=/var/www/html/wordpress \
+	--url="https://$DOMAIN_NAME" \
+	--title="Inception" \
+	--admin_user="${WP_ADMIN_USER}" \
+	--admin_password="${WP_ADMIN_PASSWORD}" \
+	--admin_email="${WP_ADMIN_EMAIL}" \
+	--skip-email
+
+wp user create \
+	--allow-root \
+	--path=/var/www/html/wordpress \
+	"${WP_USER}" \
+	"${WP_USER_EMAIL}" \
+	--user_pass="${WP_PASSWORD}" \
+	--role=author
+
+exec php-fpm -F
