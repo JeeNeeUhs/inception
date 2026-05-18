81ac1f38c522dd82455e2ff18e4a2842c98e15a7
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2026-05-18 14:49:29 +0300

    fix(nginx): update server_name in default.conf

--- srcs/requirements/nginx/entrypoint.sh
+++ srcs/requirements/nginx/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p /ssl
+
 openssl req \
 	-x509 \
 	-nodes \
@@ -7,7 +9,7 @@ openssl req \
 	-newkey rsa:2048 \
 	-keyout /ssl/nginx.key \
 	-out /ssl/nginx.crt \
-	-subj "/C=TR/ST=Turkey/L=Kocaeli/O=42/OU=ahekinci/CN=$DOMAIN_NAME"
+	-subj "/C=TR/ST=Turkey/L=Kocaeli/O=42/OU=ahekinci/CN=${DOMAIN_NAME}"
 
 echo "nginx complated"
 exec nginx -g "daemon off;"
