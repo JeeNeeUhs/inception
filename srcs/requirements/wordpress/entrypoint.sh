c5e5eb1413c2e2c2ffb11b00420f053f8d11959c
JeeNeeUhs <94013978+JeeNeeUhs@users.noreply.github.com>
2026-05-18 16:40:56 +0300

    refactor(entrypoint): restructure WordPress setup logic

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -4,31 +4,36 @@ DB_PASSWORD=$(cat /run/secrets/DB_PASSWORD) || { echo "Error: DB_PASSWORD secret
 WP_PASSWORD=$(cat /run/secrets/WP_PASSWORD) || { echo "Error: WP_PASSWORD secret not found"; exit 1; }
 WP_ADMIN_PASSWORD=$(cat /run/secrets/WP_ADMIN_PASSWORD) || { echo "Error: WP_ADMIN_PASSWORD secret not found"; exit 1; }
 
-wp config create \
-	--allow-root \
-	--path=/var/www/html/wordpress \
-	--dbname="${DB_DB}" \
-	--dbuser="${DB_USER}" \
-	--dbpass="${DB_PASSWORD}" \
-	--dbhost=mariadb \
+if [ ! -f "wp-config.php" ]; then
+	wp core download --path=/var/www/html/wordpress --allow-root
 
-wp core install \
-	--allow-root \
-	--path=/var/www/html/wordpress \
-	--url="https://$DOMAIN_NAME" \
-	--title="Inception" \
-	--admin_user="${WP_ADMIN_USER}" \
-	--admin_password="${WP_ADMIN_PASSWORD}" \
-	--admin_email="${WP_ADMIN_EMAIL}" \
-	--skip-email
+	wp config create \
+		--allow-root \
+		--path=/var/www/html/wordpress \
+		--dbname="${DB_DB}" \
+		--dbuser="${DB_USER}" \
+		--dbpass="${DB_PASSWORD}" \
+		--dbhost=mariadb \
 
-wp user create \
-	--allow-root \
-	--path=/var/www/html/wordpress \
-	"${WP_USER}" \
-	"${WP_USER_EMAIL}" \
-	--user_pass="${WP_PASSWORD}" \
-	--role=author
+	wp core install \
+		--allow-root \
+		--path=/var/www/html/wordpress \
+		--url="https://${DOMAIN_NAME}" \
+		--title="Inception" \
+		--admin_user="${WP_ADMIN_USER}" \
+		--admin_password="${WP_ADMIN_PASSWORD}" \
+		--admin_email="${WP_ADMIN_EMAIL}" \
+		--skip-email
+
+	wp user create \
+		--allow-root \
+		--path=/var/www/html/wordpress \
+		"${WP_USER}" \
+		"${WP_USER_EMAIL}" \
+		--user_pass="${WP_PASSWORD}" \
+		--role=author
+
+fi
 
 echo "WordPress installation complete."
 
